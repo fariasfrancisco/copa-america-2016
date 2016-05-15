026d88c33947b0221cf53e7568135800abc87bc2
Lucio Poveda <lucio.poveda@gmail.com>
2016-05-14 21:28:27 -0300

    adding selectOnClick directive, to autoselct text inputs on click event

---
+++ client/app/selectOnClick/selectOnClick.directive.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('copaamericaApp')
+  .directive('selectOnClick', ['$window' ,function ($window) {
+    return {
+      template: '<div></div>',
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        element.on('click', function () {
+          if (!$window.getSelection().toString()) {
+            // Required for mobile Safari
+            this.setSelectionRange(0, this.value.length)
+          }
+        });
+      }
+    };
+  }]);
