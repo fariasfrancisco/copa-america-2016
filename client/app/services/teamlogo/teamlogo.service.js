32c92138381d5feebcbd9f2114e3a3bff03ff0e9
Francisco.Farias <fariasfranciscoes@gmail.com>
2016-05-16 20:13:47 -0300

    Resized and relocated team logos into small and large images. Rewrote the service that provides their paths.

--- client/app/services/teamlogo/teamlogo.service.js
+++ client/app/services/teamlogo/teamlogo.service.js
@@ -2,29 +2,53 @@
 
 angular.module('copaamericaApp')
   .service('TeamLogoService', function () {
-    const logoPaths = [
-      'assets/images/team_logos/0.png',
-      'assets/images/team_logos/1.png',
-      'assets/images/team_logos/2.png',
-      'assets/images/team_logos/3.png',
-      'assets/images/team_logos/4.png',
-      'assets/images/team_logos/5.png',
-      'assets/images/team_logos/6.png',
-      'assets/images/team_logos/7.png',
-      'assets/images/team_logos/8.png',
-      'assets/images/team_logos/9.png',
-      'assets/images/team_logos/10.png',
-      'assets/images/team_logos/11.png',
-      'assets/images/team_logos/12.png',
-      'assets/images/team_logos/13.png',
-      'assets/images/team_logos/14.png',
-      'assets/images/team_logos/15.png',
-      'assets/images/team_logos/q.png'
+    const largeLogoPaths = [
+      'assets/images/team_logos/large/0.png',
+      'assets/images/team_logos/large/1.png',
+      'assets/images/team_logos/large/2.png',
+      'assets/images/team_logos/large/3.png',
+      'assets/images/team_logos/large/4.png',
+      'assets/images/team_logos/large/5.png',
+      'assets/images/team_logos/large/6.png',
+      'assets/images/team_logos/large/7.png',
+      'assets/images/team_logos/large/8.png',
+      'assets/images/team_logos/large/9.png',
+      'assets/images/team_logos/large/10.png',
+      'assets/images/team_logos/large/11.png',
+      'assets/images/team_logos/large/12.png',
+      'assets/images/team_logos/large/13.png',
+      'assets/images/team_logos/large/14.png',
+      'assets/images/team_logos/large/15.png',
+      'assets/images/team_logos/large/q.png'
+    ];
+    
+    const smallLogoPaths = [
+      'assets/images/team_logos/small/0.png',
+      'assets/images/team_logos/small/1.png',
+      'assets/images/team_logos/small/2.png',
+      'assets/images/team_logos/small/3.png',
+      'assets/images/team_logos/small/4.png',
+      'assets/images/team_logos/small/5.png',
+      'assets/images/team_logos/small/6.png',
+      'assets/images/team_logos/small/7.png',
+      'assets/images/team_logos/small/8.png',
+      'assets/images/team_logos/small/9.png',
+      'assets/images/team_logos/small/10.png',
+      'assets/images/team_logos/small/11.png',
+      'assets/images/team_logos/small/12.png',
+      'assets/images/team_logos/small/13.png',
+      'assets/images/team_logos/small/14.png',
+      'assets/images/team_logos/small/15.png',
+      'assets/images/team_logos/small/q.png'
     ];
 
     return {
-      getPath(index) {
-        return logoPaths[index];
+      getSmallPath(index) {
+        return smallLogoPaths[index];
+      },
+      
+      getLargePath(index) {
+        return largeLogoPaths[index];
       }
     };
   });
